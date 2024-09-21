c499bbba4257d0ea5da42ee9727aa0091d4e8bad
Andre Torres <atcorrea17@gmail.com>
2024-09-21 18:00:52 -0300

    test build breaker

--- IaC/database_user.tf
+++ IaC/database_user.tf
@@ -1,6 +1,4 @@
 resource "mongodbatlas_database_user" "user" {
-  username           = var.dbuser
-  password           = var.dbuser_password
   project_id         = mongodbatlas_project.project.id
   auth_database_name = "admin"
 
